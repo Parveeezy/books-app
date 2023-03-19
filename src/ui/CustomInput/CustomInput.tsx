dd700af7b293f803180806c0bff6737abbf513a4
Parviz Shirinov <parveeezy@gmail.com>
2023-03-19 20:51:23 +0300

    first commit

---
+++ src/ui/CustomInput/CustomInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {CustomInpt} from "./components";
+
+const CustomInput = () => {
+    return (
+        <div>
+            <CustomInpt fullWidth label="fullWidth" id="fullWidth" />
+        </div>
+    );
+};
+
+export default CustomInput;
---
+++ src/ui/CustomInput/components.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import {TextField} from "@mui/material";
+
+export const CustomInpt = styled(TextField)`
+  
+`
