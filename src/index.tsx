69767ac331857d702373edea3489ee624d41e3ee
Parviz Shirinov <parveeezy@gmail.com>
2024-01-19 13:44:10 +0300

    fix route trouble

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 
+const router = createBrowserRouter([
+    {
+        path: "/books-app",
+        element: <App />,
+    },
+]);
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>,
 );
